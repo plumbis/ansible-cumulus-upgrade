594f38c6b14c1daecd73e69de6e66a9d8095549c
kwitherstine <kwitherstine@gmail.com>
2016-05-11 20:57:20 -0500

    l2demo now has working config backup options and the file copy in the leaf-upgrade has been updated to at least do the copy.

--- l2-demo/under_construction/l2-troubleshooting.sh
+++
@@ -1,8 +0,0 @@
-# not yet functional, having a hard time getting this to work against the vagrant environment
-#! /bin/bash
-#!/bin/bash
-HOSTS="leaf1 leaf2 leaf3 leaf4 leaf5 leaf6 spine1 spine2"
-SCRIPT="hostname; sudo clagctl; sudo brctl show; sudo brctl showmacs br0; sudo brctl showstp br0; sudo ifquery -a; sudo ip link show; exit"
-for HOSTNAME in ${HOSTS} ; do
-    vagrant ssh ${HOSTNAME} "${SCRIPT}"
-done
